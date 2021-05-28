5df2da8c636fa0ab815ab889756f3764fee0fb37
EvanLegault <Legaul26Ev@lycee-ndduroc.com>
2021-05-27 23:57:49 -0700

    api

---
+++ app/Models/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Users extends \Illuminate\Database\Eloquent\Model
+{
+    use HasFactory;
+    public $timestamps = false;
+}
